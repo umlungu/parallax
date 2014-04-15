c07dfef45662e285a8e7f8effcdbec14f55a97fd
Felix Norton <felix@succinctdesign.co.za>
2014-04-15 19:51:55 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Parallax::Application.config.secret_key_base = 'f26707bbac6dc092bdc894dffee944a55ef5ec088d1aba91a434a8bc78bcf6a66b5e699e73b69cfcc38dab38376b1cb3d1b0183a13644526d6e473ff0bb7aea1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Parallax::Application.config.session_store :cookie_store, key: '_parallax_session'
